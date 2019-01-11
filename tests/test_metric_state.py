d086338883f081b8f3125edc7913c3638bd6a4f7
neumond <knifeslaughter@gmail.com>
2019-01-11 20:42:03 +0300

    Attempt to write examples in tests

--- tests/test_metric_state.py
+++ tests/test_metric_state.py
@@ -1,27 +1,66 @@
+import pytest
+
 from epimetheus import Counter, Gauge, Histogram, Summary
 
 
 def test_counter():
     c = Counter(name='name')
+
     assert c.get_output() == {
         'count': 0,
     }
-    assert list(c.expose()) == [
-        'name 0',
-    ]
+    assert list(c.expose()) == ['name 0']
+
     c.inc()
     assert c.get_output() == {
         'count': 1,
     }
+    assert list(c.expose()) == ['name 1']
+
     c.inc()
     c.inc()
     assert c.get_output() == {
         'count': 3,
     }
+    assert list(c.expose()) == ['name 3']
+
     c.inc(10)
     assert c.get_output() == {
         'count': 13,
     }
+    assert list(c.expose()) == ['name 13']
+
+
+def test_counter_with_labels():
+    c = Counter(name='name').with_labels(key='value', x='300')
+    c.inc(45)
+    assert list(c.expose()) == ['name{key="value",x="300"} 45']
+
+
+@pytest.mark.skip
+def test_many_labels():
+    base = Counter(name='name')
+    base.inc(2)
+    a = base.with_labels(a='1')
+    a.inc(3)
+    b = base.with_labels(b='2')
+    b.inc(4)
+    bsub = b.with_labels(s='_')
+    bsub.inc(5)
+    c = base.with_labels(c='3')
+    c.inc(6)
+
+    assert list(base.expose()) == ['name 2']
+    assert list(a.expose()) == ['name{a="1"} 3']
+    assert list(b.expose()) == ['name{b="2"} 4']
+    assert list(bsub.expose()) == ['name{b="2",s="_"} 5']
+    assert list(c.expose()) == ['name{c="3"} 6']
+
+
+@pytest.mark.skip
+def test_with_timestamp():
+    c = Counter(name='name')
+    c.supply_timestamp(True)
 
 
 def test_gauge():
